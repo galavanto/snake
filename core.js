ce4b075fe41e1168915f2e3c83f1ec76d6951091
galavanto <martijnb50@gmail.com>
2020-07-19 15:07:57 +0200

    initiele setup gemakt

---
+++ core.js
@@ -0,0 +1,75 @@
+let Game = {
+
+    interval: 1000 / 15,
+    paused: false,
+
+    init: function () {
+        window.setInterval(this.core, this.interval);
+        this.drawCanvas();
+        this.bindKeys();
+    },
+    bindKeys: function () {
+        window.addEventListener("keydown", this.hitKey);
+    },
+    drawCanvas: function () {
+        let canvas = document.createElement('canvas');
+        canvas.id = 'gameCanvas';
+        canvas.style.width = "500px";
+        canvas.style.height = "500px";
+        canvas.style.backgroundColor = "black";
+
+        document.body.appendChild(canvas);
+    },
+    core: function () {
+        if (this.paused) {
+            this.showPauseMenu()
+            console.log('paused');
+            return;
+        }
+        console.log('tick');
+    },
+    hitKey: function (event) {
+        if (event.defaultPrevented) {
+            return;
+        }
+
+        switch (event.key) {
+            case "Down":
+            case "ArrowDown":
+
+                break;
+            case "Up":
+            case "ArrowUp":
+
+                break;
+            case "Left":
+            case "ArrowLeft":
+
+                break;
+            case "Right":
+            case "ArrowRight":
+
+                break;
+            case "Enter":
+
+                break;
+            case "Esc":
+            case "Escape":
+                if (!this.paused) {
+                    this.paused = true;
+                    break;
+                }
+
+                this.paused = false;
+                break;
+            default:
+                return;
+        }
+
+        // Cancel the default action to avoid it being handled twice
+        event.preventDefault();
+    },
+    showPauseMenu: function(){
+
+    }
+};
---
+++ snake.js
@@ -0,0 +1,9 @@
+let Snake = {
+    length: 0,
+    x: 0,
+    y: 0,
+
+    init: function(){
+
+    }
+};
